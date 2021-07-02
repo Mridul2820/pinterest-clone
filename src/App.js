027a6c1d7e3db40007c9770032bb18b371b4f1e7
Mridul2820 <immridulpanda2820@gmail.com>
2021-07-03 00:01:50 +0530

    global styles add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import GlobalStyle from './GlobalStyles'
 
 const App = () => {
     return (
         <div>
+            <GlobalStyle />
             Pinterest Clone
         </div>
     )
